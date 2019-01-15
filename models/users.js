a31badfccde90a7d7e97c7adc78c1c1f72227379
darryldriedger <darryldriedger@gmail.com>
2019-01-15 02:50:47 -0700

    added models and views folders

---
+++ models/users.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var UserSchema = new mongoose.Schema({
+    emailAddress: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+});
+            // // authenticate input against database documents
+            // UserSchema.statics.authenticate = function(email, password, callback) {
+            //   User.findOne({ email: email })
+            //       .exec(function (error, user) {
+            //         if (error) {
+            //           return callback(error);
+            //         } else if ( !user ) {
+            //           var err = new Error('User not found.');
+            //           err.status = 401;
+            //           return callback(err);
+            //         }
+            //         bcrypt.compare(password, user.password , function(error, result) {
+            //           if (result === true) {
+            //             return callback(null, user);
+            //           } else {
+            //             return callback();
+            //           }
+            //         })
+            //       });
+            // }
+            // // hash password before saving to database
+            // UserSchema.pre('save', function(next) {
+            //   var user = this;
+            //   bcrypt.hash(user.password, 10, function(err, hash) {
+            //     if (err) {
+            //       return next(err);
+            //     }
+            //     user.password = hash;
+            //     next();
+            //   })
+            // });
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
