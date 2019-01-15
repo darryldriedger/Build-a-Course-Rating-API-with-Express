d5840c15073009fea021565c04fb28c00eaf3c45
darryldriedger <darryldriedger@gmail.com>
2019-01-15 02:59:23 -0700

    added pug index and layout views; rendered views in main page route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 // send a friendly greeting for the root route
 router.get('/', (req, res) => {
-  return res.send('Welcome to the Course Review API, Buckle up!');
+  //return res.send('Welcome to the Course Review API, Buckle up!');
+  return res.render('index', { title: 'Home' });
 });
 
 
