6ca3b86eb18d7ef5c656232e2845c34cbb71757c
Hairy Bertson <hairybertson@localhost.localdomain>
2019-02-27 22:56:42 -0500

    added output and profile to views; testing code in routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
 
 
 // send a friendly greeting for the root route
@@ -8,12 +9,39 @@ router.get('/', (req, res) => {
   return res.render('index', { title: 'Home' });
 });
 
-//get all books and set pagination offset to zero
+//get
 router.get('/api/users200', function(req, res, next) {
-  res.send("Returns the currently authenticated user");
+  // res.render('output', { title: 'Test page' })
+  // res.send("Returns the currently authenticated user");
+  User.find()
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          // res.send("this is tester text");
+          // console.log(User.find());
+          // return res.render('output', { title: 'Test page' })
+          return res.render('profile', { title: 'Profile', name: user.fullName, address: user.emailAddress, password: user.password });
+        }
+      });
+
+
 });
 
-/* PUT Edit/change book details. PUT PUT PUT PUT PUT  */
+
+// router.get('/profile', mid.requiresLogin, function(req, res, next) {
+//   User.findById(req.session.userId)
+//       .exec(function (error, user) {
+//         if (error) {
+//           return next(error);
+//         } else {
+//           return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
+//         }
+//       });
+// });
+
+
+/* PUT PUT PUT PUT PUT PUT  */
 router.post('/api/users201', function(req, res, next){
   res.send("Creates a user, sets the Location header to '/', and returns no content");
 });
