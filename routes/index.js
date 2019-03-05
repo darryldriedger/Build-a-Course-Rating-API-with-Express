ceb3a99311a8f3dad85231681bc108ac28222e9a
Hairy Bertson <hairybertson@localhost.localdomain>
2019-03-05 11:37:16 -0500

    worked on profile route to render users collection from mongodb

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ var User = require('../models/users');
 // send a friendly greeting for the root route
 router.get('/', (req, res) => {
   //return res.send('Welcome to the Course Review API, Buckle up!');
+  console.log(__dirname);
   return res.render('index', { title: 'Home' });
+  // res.sendFile(__dirname + '/index.html');
 });
 
 //get
@@ -19,7 +21,6 @@ router.get('/api/users200', function(req, res, next) {
           return next(error);
         } else {
           // res.send("this is tester text");
-          // console.log(User.find());
           // return res.render('output', { title: 'Test page' })
           return res.render('profile', { title: 'Profile', name: user.fullName, address: user.emailAddress, password: user.password });
         }
@@ -28,17 +29,21 @@ router.get('/api/users200', function(req, res, next) {
 
 });
 
-
-// router.get('/profile', mid.requiresLogin, function(req, res, next) {
-//   User.findById(req.session.userId)
-//       .exec(function (error, user) {
-//         if (error) {
-//           return next(error);
-//         } else {
-//           return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
-//         }
-//       });
-// });
+router.get('/profile', function(req, res, next) {
+  User.find()
+      .exec(function (error, results) {
+        if (error) {
+          return next(error);
+        } else {
+          let users = results;
+          console.log(users);
+          // const user = user[0];
+        return res.render('profile', { title: 'Users', users: users });
+          // return res.render('profile', { title: 'Profile'})
+        res.send("this is the profile page text");
+        }
+      });
+});
 
 
 /* PUT PUT PUT PUT PUT PUT  */
