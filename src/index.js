e83d91b07a0bf60492f5de6b172d957cb034fd45
Hairy Bertson <hairybertson@localhost.localdomain>
2019-03-05 11:41:23 -0500

    added express-session and connect-mongo to src/index

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 const app = express();
 
 // mongodb connection
-mongoose.connect("mongodb://localhost:27017/course-api", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/course-api", { useCreateIndex: true, useNewUrlParser: true });
 const db = mongoose.connection;
 // mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -28,12 +29,13 @@ var routes = require('../routes/index');
 app.use('/', routes);
 var usersRoute = require('../routes/users');
 app.use('/users', usersRoute);
-// // send a friendly greeting for the root route
-// app.get('/', (req, res) => {
-//   res.json({
-//     message: 'Welcome to the Course Review API'
-//   });
-// });
+
+                                              // // send a friendly greeting for the root route
+                                              // app.get('/', (req, res) => {
+                                              //   res.json({
+                                              //     message: 'Welcome to the Course Review API'
+                                              //   });
+                                              // });
 
 
 // parse incoming requests
@@ -42,12 +44,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve static files from /public
 app.use(express.static(__dirname + '/../public'));
+
 // view engine setup
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/../views');
-
-// serve static files from /public
-app.use(express.static(__dirname + '/public'));
 
 // uncomment this route in order to test the global error handler
 // app.get('/error', function (req, res) {
@@ -72,5 +72,5 @@ app.use((err, req, res, next) => {
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
+  console.log(`Express server is running on port ${server.address().port}`);
 });
